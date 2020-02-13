70aea264344cac9aa31b807d1c6b9ee09032081a
Olli Stenroos <olli.stenroos@tuni.fi>
2020-02-14 01:44:26 +0200

    Add startup and NUnit projects

---
+++ FJ/FJ.Desktop.Debug/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FJ.Desktop.Debug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
