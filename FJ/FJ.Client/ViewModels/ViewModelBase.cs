ced601f2a87d071e57fd4a165c9dcb9cb18c0e92
Olli Stenroos <olli.stenroos@tuni.fi>
2020-02-14 03:04:05 +0200

    ViewModelBase: Implementation

--- FJ/FJ.Client/ViewModels/ViewModelBase.cs
+++ FJ/FJ.Client/ViewModels/ViewModelBase.cs
@@ -1,11 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Prism.Regions;
 using ReactiveUI;
 
 namespace FJ.Client.ViewModels
 {
-    public class ViewModelBase : ReactiveObject
+    public class ViewModelBase : ReactiveObject, IRegionMemberLifetime, IJournalAware, INavigationAware
     {
+        #region IRegionMemberLifetime
+        public virtual bool KeepAlive => true;
+        #endregion
+
+        #region IJournalAware
+        public virtual bool PersistInHistory()
+        {
+            return true;
+        }
+        #endregion
+
+        #region INavigationAware
+        public virtual bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+
+        public virtual void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+        }
+
+        public virtual void OnNavigatedTo(NavigationContext navigationContext)
+        {
+        }
+        #endregion
+
+        public virtual void DoRefresh()
+        {
+        }
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            ((IReactiveObject)this).RaisePropertyChanged(propertyName);
+        }
+
+        protected void RaisePropertiesChanged()
+        {
+            RaisePropertyChanged(null);
+        }
     }
 }
