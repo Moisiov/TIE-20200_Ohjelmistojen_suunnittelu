a2841e1aa079846a047e81fdbd13a8e89ecadeb9
Olli Stenroos <olli.stenroos@tuni.fi>
2020-02-15 14:44:11 +0200

    Client: Make project non-executable

---
+++ FJ/FJ.Client/FJClientEntryPoint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Dialogs;
+using Avalonia.Logging.Serilog;
+using Avalonia.ReactiveUI;
+using Unity;
+
+namespace FJ.Client
+{
+    public static class FJClientEntryPoint
+    {
+        [STAThread]
+        public static void RunApplication(IUnityContainer container, string[] args)
+        {
+            var avaloniaAppBuilder = BuildAvaloniaApp();
+            // TODO inject container
+            avaloniaAppBuilder.StartWithClassicDesktopLifetime(args);
+        }
+
+        // Avalonia configuration, don't remove; also used by visual designer.
+        private static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToDebug()
+                .UseReactiveUI()
+                .UseManagedSystemDialogs();
+    }
+}
