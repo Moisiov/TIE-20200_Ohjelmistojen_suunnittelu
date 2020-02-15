c9ff0a8e878bb0916f049d658593d87e1baaca81
Olli Stenroos <olli.stenroos@tuni.fi>
2020-02-15 22:27:35 +0200

    Startup: Add new registrations
    * Improve readability as well

--- FJ/FJ.Desktop.Debug/Registrations.cs
+++ FJ/FJ.Desktop.Debug/Registrations.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using FJ.Client.Services;
+using FJ.FinlandiaHiihtoAPI;
+using FJ.ServiceInterfaces.FinlandiaHiihto;
+using FJ.Services.FinlandiaHiihto;
+using FJ.Services.FinlandiaHiihto.FinlandiaDataFetchingServices;
 using Unity;
 
 namespace FJ.Desktop.Debug
@@ -8,18 +12,31 @@ namespace FJ.Desktop.Debug
     {
         public static void DoRegistrations(IUnityContainer container)
         {
-            ClientRegistrations(container);
-            ServicesRegistrations(container);
+            ClientInternalRegistrations(container);
+            ServicesInternalRegistrations(container);
+            ServicesExternalRegistratios(container);
+            ServiceInterfacesRegistrations(container);
         }
 
-        private static void ClientRegistrations(IUnityContainer container)
+        private static void ClientInternalRegistrations(IUnityContainer container)
         {
             container.RegisterSingleton<IContentRegionNavigator, ContentRegionNavigator>();
         }
 
-        private static void ServicesRegistrations(IUnityContainer container)
+        private static void ServicesInternalRegistrations(IUnityContainer container)
         {
-            // TODO
+            container.RegisterSingleton<IDataFetchingService, FinlandiaAPIDataFetchingService>();
+        }
+
+        private static void ServicesExternalRegistratios(IUnityContainer container)
+        {
+            // APIs are treated as externals
+            container.RegisterInstance<IFinlandiaHiihtoAPI>(new FinlandiaHiihtoAPI.FinlandiaHiihtoAPI());
+        }
+
+        private static void ServiceInterfacesRegistrations(IUnityContainer container)
+        {
+            container.RegisterType<ILatestFinlandiaResultsService, LatestFinlandiaResultsService>();
         }
     }
 }
