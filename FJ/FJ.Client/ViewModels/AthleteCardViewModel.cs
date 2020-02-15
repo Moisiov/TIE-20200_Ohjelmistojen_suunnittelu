934a1b195788984caa74ee3326bcb7e58e67a093
Olli Stenroos <olli.stenroos@tuni.fi>
2020-02-15 22:21:03 +0200

    AthleteCardView: Add basic implementation

---
+++ FJ/FJ.Client/ViewModels/AthleteCardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FJ.Client.Models;
+
+namespace FJ.Client.ViewModels
+{
+    public class AthleteCardViewModel : ViewModelBase
+    {
+        private AthleteCardModel m_model;
+
+        public AthleteCardViewModel()
+        {
+            m_model = new AthleteCardModel();
+        }
+    }
+}
