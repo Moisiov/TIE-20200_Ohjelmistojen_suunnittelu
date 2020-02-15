0215648ad7f563fdc92483bfb6b2db1b2df3730c
Olli Stenroos <olli.stenroos@tuni.fi>
2020-02-15 22:30:17 +0200

    FrontPage: Add navigations to newly added views

--- FJ/FJ.Client/ViewModels/FrontPageViewModel.cs
+++ FJ/FJ.Client/ViewModels/FrontPageViewModel.cs
@@ -1,15 +1,39 @@
 ﻿using System;
 using FJ.Client.Models;
+using FJ.Client.Services;
+using FJ.Client.Views;
 
 namespace FJ.Client.ViewModels
 {
     public class FrontPageViewModel : ViewModelBase
     {
+        private readonly IContentRegionNavigator m_navigator;
         private FrontPageModel m_model;
 
-        public FrontPageViewModel()
+        public FrontPageViewModel(IContentRegionNavigator navigator)
         {
+            m_navigator = navigator;
             m_model = new FrontPageModel();
+        }
+
+        public void NavigateToResultRegister()
+        {
+            m_navigator.DoNavigateTo<ResultRegisterView>();
+        }
+
+        public void NavigateToCompetitionGeneral()
+        {
+            m_navigator.DoNavigateTo<CompetitionGeneralView>();
+        }
+
+        public void NavigateToAthleteCard()
+        {
+            m_navigator.DoNavigateTo<AthleteCardView>();
+        }
+
+        public void NavigateToTeamCard()
+        {
+            m_navigator.DoNavigateTo<TeamCardView>();
         }
     }
 }
