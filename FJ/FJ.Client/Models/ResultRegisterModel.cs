28d2b421732eedc39ec30f34151828d58dc4d085
Olli Stenroos <olli.stenroos@tuni.fi>
2020-02-15 22:29:50 +0200

    ResultRegisterView: Add basic implementation
    * Also works as a proof of concept for getting Finlandia results to UI

---
+++ FJ/FJ.Client/Models/ResultRegisterModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FJ.DomainObjects.FinlandiaHiihto;
+using FJ.DomainObjects.FinlandiaHiihto.Enums;
+using FJ.ServiceInterfaces.FinlandiaHiihto;
+
+namespace FJ.Client.Models
+{
+    public class ResultRegisterModel
+    {
+        private readonly ILatestFinlandiaResultsService m_latestFinlandiaResultsService;
+
+        public ResultRegisterModel(ILatestFinlandiaResultsService latestFinlandiaResultsService)
+        {
+            m_latestFinlandiaResultsService = latestFinlandiaResultsService;
+        }
+
+        public async Task<IEnumerable<string>> GetLatestFinlandiaResultsAsSortedStrings()
+        {
+            // TODO this is just a proof of concept
+            var collection = await m_latestFinlandiaResultsService.GetLatestFinlandiaResults();
+            var res = collection.Results
+                .Where(x => x.Distance == FinlandiaSkiingDistance.Fifty && x.Style == FinlandiaSkiingStyle.Classic)
+                .OrderBy(x => x.PositionGeneral)
+                .Take(100)
+                .Select(x => $"{x.PositionGeneral}\t{x.FullName}");
+
+            return res;
+        }
+    }
+}
---
+++ FJ/FJ.Client/ViewModels/ResultRegisterViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Reactive;
+using System.Threading.Tasks;
+using FJ.Client.Events;
+using FJ.Client.Models;
+using FJ.ServiceInterfaces.FinlandiaHiihto;
+using Prism.Events;
+using ReactiveUI;
+
+namespace FJ.Client.ViewModels
+{
+    public class ResultRegisterViewModel : ViewModelBase
+    {
+        private ResultRegisterModel m_model;
+
+        public ReactiveCommand<Unit, Unit> TestCommand { get; }
+        public ObservableCollection<string> Results { get; set; }
+
+        public ResultRegisterViewModel(IEventAggregator ea, ILatestFinlandiaResultsService latestFinlandiaResultsService)
+        {
+            m_model = new ResultRegisterModel(latestFinlandiaResultsService);
+
+            ea.GetEvent<ContentRegionRefreshRequestedEvent>().Subscribe(DoRefresh);
+
+            TestCommand = ReactiveCommand.CreateFromTask(TestCall);
+            Results = new ObservableCollection<string>();
+        }
+
+        public async Task TestCall()
+        {
+            var res = await m_model.GetLatestFinlandiaResultsAsSortedStrings();
+            Results = new ObservableCollection<string>(res);
+            RaisePropertyChanged(nameof(Results));
+        }
+
+        public override void DoRefresh()
+        {
+            Results = new ObservableCollection<string>();
+            RaisePropertyChanged(nameof(Results));
+        }
+    }
+}
