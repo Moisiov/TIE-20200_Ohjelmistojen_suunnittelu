f89c2dfd14e6240ee8228a0659b2f75ca5015e43
Olli Stenroos <olli.stenroos@tuni.fi>
2020-03-09 20:16:54 +0200

    Utils: Add extension to get enum description from attribute

---
+++ FJ/FJ.Utils/EnumExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace FJ.Utils
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Get the Description of enum's DescriptionAttribute
+        /// </summary>
+        /// <param name="enumValue">Enum of which description is looked for</param>
+        /// <returns>Description of this enum</returns>
+        public static string GetDescription(this Enum enumValue)
+        {
+            return enumValue.GetType()
+                            .GetMember(enumValue.ToString())[0]
+                            .GetCustomAttribute<DisplayAttribute>()
+                            .GetDescription();
+        }
+    }
+}
