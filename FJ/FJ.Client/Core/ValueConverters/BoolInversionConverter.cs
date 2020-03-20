2dff36edbc6fd7f22299a3447ff16f4cf5120eed
Olli Stenroos <olli.stenroos@tuni.fi>
2020-03-20 13:16:15 +0200

    Client: Use bool inversion converter instead of explicit binding

---
+++ FJ/FJ.Client/Core/ValueConverters/BoolInversionConverter.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace FJ.Client.Core.ValueConverters
+{
+    public class BoolInversionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool booleanValue)
+            {
+                return !booleanValue;
+            }
+
+            throw new ArgumentException(nameof(value));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
