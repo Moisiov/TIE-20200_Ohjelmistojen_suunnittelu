44cecea5b1e8565074f58a3382cf83180fc4ec1b
Moisiov <39945423+Moisiov@users.noreply.github.com>
2020-04-18 18:30:32 +0300

    ControlPanelView: Add simple clock and temperature

--- FJ/FJ.Client/ControlPanel/ControlPanelViewModel.cs
+++ FJ/FJ.Client/ControlPanel/ControlPanelViewModel.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Reactive;
+using Avalonia.Threading;
 using FJ.Client.Athlete;
 using FJ.Client.CompetitionGeneral;
 using FJ.Client.Core;
@@ -7,20 +11,38 @@ using FJ.Client.Core.Services;
 using FJ.Client.FrontPage;
 using FJ.Client.ResultRegister;
 using FJ.Client.Team;
+using IlmatieteenLaitosAPI;
+using ReactiveUI;
 
 namespace FJ.Client.ControlPanel
 {
     public class ControlPanelViewModel : ViewModelBase
     {
         private readonly IControlPanelRegionController m_controlPanelRegionController;
+        private readonly IIlmatieteenLaitosAPI m_ilmatieteenLaitosAPI;
 
         public bool IsExpanded => m_controlPanelRegionController.CurrentControlPanelSizeOption == ControlPanelSizeOption.Expanded;
+
+        public ReactiveCommand<Unit, Unit> GetWeatherCommand { get; }
+        public string CurrentTime { get; set; }
+        public string CurrentTemperature { get; set; }
+        public string CurrentWindSpeed { get; set; }
 
         private ControlPanelModel m_model;
 
         public ControlPanelViewModel(IControlPanelRegionController controPanelRegionController)
         {
             m_controlPanelRegionController = controPanelRegionController;
+
+            m_ilmatieteenLaitosAPI = new IlmatieteenLaitosAPI.IlmatieteenLaitosAPI();
+            GetWeatherCommand = ReactiveCommand.CreateFromTask(GetCurrentWeather);
+            GetWeatherCommand.Execute().Subscribe();
+            DispatcherTimer weatherTimer = new DispatcherTimer(TimeSpan.FromMinutes(10), DispatcherPriority.Normal, OnWeatherTimerTick);
+            weatherTimer.Start();
+
+            RefreshTimeText();
+            DispatcherTimer clockTimer = new DispatcherTimer(TimeSpan.FromSeconds(1), DispatcherPriority.Normal, OnClockTimerTick);
+            clockTimer.Start();
 
             m_model = new ControlPanelModel();
         }
@@ -60,6 +82,29 @@ namespace FJ.Client.ControlPanel
         public void NavigateToTeamCard()
         {
             Navigator.DoNavigateTo<TeamCardView>();
+        }
+
+        private void OnWeatherTimerTick(object sender, EventArgs e)
+        {
+            GetWeatherCommand.Execute().Subscribe();
+        }
+
+        private void OnClockTimerTick(object sender, EventArgs e)
+        {
+            RefreshTimeText();
+        }
+
+        private void RefreshTimeText()
+        {
+            CurrentTime = DateTime.Now.ToString("HH':'mm");
+            RaisePropertyChanged(nameof(CurrentTime));
+        }
+
+        private async Task GetCurrentWeather()
+        {
+            var weatherData = await m_ilmatieteenLaitosAPI.GetWeatherNow("Lahti");
+            CurrentTemperature = weatherData.AirTemperature.HasValue ? weatherData.AirTemperature.Value.ToString("0.0", CultureInfo.InvariantCulture) + " °C" : "";
+            RaisePropertyChanged(nameof(CurrentTemperature));
         }
     }
 }
